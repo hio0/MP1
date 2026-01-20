2fdf11b6a0fb7f7e74158cece735094aceaab0eb
hio0 <hioohio1234@naver.com>
2026-01-20 20:03:44 +0900

    Text와 Save PROJECT

---
+++ Assets/3. Scripts/MainManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1. 슬라이딩 필드 만들기
+        //2. 글자 변경 툴 만들기
+        //3. 목차 프리팹 만들기
+        //4. 저장 만들기
+    }
+}
