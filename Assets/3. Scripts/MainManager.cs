47e2802115c7bf26ac02e2cfe1d63f73811d13d0
hio0 <hioohio1234@naver.com>
2026-04-23 22:19:44 +0900

    안해

--- Assets/3. Scripts/MainManager.cs
+++ Assets/3. Scripts/MainManager.cs
@@ -27,6 +27,7 @@ public class MainManager : MonoBehaviour
 
     string open;
     string close;
+    public static World you;
 
     [System.Serializable]
     public class Data
@@ -72,27 +73,39 @@ public class MainManager : MonoBehaviour
 
     public void OnStyle(string st)
     {
-        if (select != null)
+        if (select != null) // 드래그 영역에 스타일 넣기
         {
-            string what = $"<{st}>";
-            Color c = gameObject.GetComponent<UnityEngine.UI.Image>().color;
+            string ovTwhat = $"<{st}>";
+            string fnLwhat = $"</{st}>";
 
-            if (select.Contains(what))
+            if (select.Contains(ovTwhat) && select.Contains(fnLwhat)) // 정직하게 있는거 빼러온 놈
             {
-                c.a = 76f;
-
-                open.Replace(what, "");
-                close.Replace($"/{what}", "");
+                open = open.Replace(ovTwhat, "");
+                close = close.Replace(fnLwhat, "");
             }
-            else
+            else // 아무것도 없다
             {
-                open += what;
-                close = $"/{what}" + close;
-
-                c.a = 105f;
+                if (you.IsIt(st)) // 근범위 내에 받았음에도 또 받으려 오는 간악한 스슝좍이 있을거다
+                {
+                    open = open.Replace(ovTwhat, ""); // 뻬버려(지금 작동 안됨)
+                    close = close.Replace(fnLwhat, "");
+                }
+                else // 아니었으니 진짜 받으러 온 놈
+                {
+                    open += ovTwhat; // 나중에 들어오면 뒤로 정렬되기에 별도의 식 필요 없음.
+                    close = fnLwhat + close; // 나중에 들어온게 안으로 들어가야하기에 
+                }
             }
+
+            // 여기가 멘탈 나간 부분입니다 이 코드는 드래그 대상이 같은 때만 상정해놓고 open이랑 close의 초기화를 안 해 놨습니다. 초기화를 해주면 되지 않냐, 할 수도 있지만...이전에 드래그했던 영역을 다시 드래그하면 그 때의 open과 close를 다시 불러와주게 하는 저장 시스템도 필요한데 유니티 기능으론 진짜 물리적으로 불가능합니다. 유니티로 메모장을 만드려 했던 것은, 완전히 실패한 발상이였습니다. 
 
             select = open + select + close;
+            you.iF.text = you.iF.text.Replace(you.selectRange, select);
+            select = null;
+        }
+        else // 드래그 없을 경우 그냥 쭈욱
+        {
+
         }
     }
 
--- Assets/3. Scripts/World.cs
+++ Assets/3. Scripts/World.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using static UnityEditor.Progress;
 using UnityEngine.UI;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 public class World : MonoBehaviour
 {
@@ -11,9 +12,13 @@ public class World : MonoBehaviour
     public float startyougjiL;
     public TMP_Text memotext;
     public RectTransform yougjiLengh;
-    TMP_InputField iF;
+    public TMP_InputField iF;
 
     public string selectRange;
+    public string selectRangeP;
+
+    int a;
+    int b;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,11 +47,61 @@ public class World : MonoBehaviour
 
         if(iF.selectionAnchorPosition > 0 && iF.selectionFocusPosition != iF.selectionAnchorPosition) // 드래그 되었다
         {
-            int a = Mathf.Min(iF.selectionAnchorPosition, iF.selectionFocusPosition); // 작은 쪽
-            int b = Mathf.Max(iF.selectionAnchorPosition, iF.selectionFocusPosition); // 큰 쪽
+            selectRangeP = null;
+
+            a = Mathf.Min(iF.selectionAnchorPosition, iF.selectionFocusPosition); // 작은 쪽
+            b = Mathf.Max(iF.selectionAnchorPosition, iF.selectionFocusPosition); // 큰 쪽
 
             selectRange = iF.text.Substring(a, b - a); // 드레그의 텍스트 가져오기(앞으로 드레그하던, 뒤로 드래그하던 동일하게,) 
             select = selectRange;
+
+            you = gameObject.GetComponent<World>();
+        }
+    }
+
+    public bool IsIt(string st)
+    {
+        string c = "";
+        string d = "";
+        string ovTwhat = $"<{st}>";
+        string fnLwhat = $"</{st}>";
+
+        for (int i = a; i > 0; i--)
+        {
+            c = iF.text.Substring(i, a);
+
+            if (c.Contains(ovTwhat))
+            {
+                break; // c는 a 뒤의 문자열을 스타일을 여는 곳 까지 저장.
+            }
+            else if (c.Contains(fnLwhat) || c.Length == 1)
+            {
+                c = "";
+                break; // 닫는 것에 닿았거나 끝까지 갔으면 이 드래그 영역엔 없다. 
+            }
+        }
+        for (int i = b; i < iF.text.Length; i++)
+        {
+            d = iF.text.Substring(b, i);
+
+            if (d.Contains(fnLwhat))
+            {
+                break;
+            }
+            else if (d.Contains(ovTwhat) || d.Length == iF.text.Length - 1)
+            {
+                d = "";
+                break;
+            }
+        }
+        if (c.Length != 0 && d.Length != 0)
+        {
+            selectRangeP = iF.text.Substring(c.Length, d.Length); // 선택 범위보다 조금 더 넓은 범위(스타일이 있는 곳 까지)
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
